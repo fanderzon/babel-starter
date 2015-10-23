bcd840f23fba8891f4d58e5af28ada55efddb088
Fredrik Andersson <fredrik.andersson@widespace.com>
2015-10-23 17:04:00 +0200

    Test commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 var gulp = require("gulp");
-var babel = require("gulp-babel");
+var $ = require('gulp-load-plugins')();
+
+var DEST = 'dist',
+    DEST_JS = DEST  + '/js',
+    DEST_VENDOR = DEST + '/vendor',
+    VENDOR_FILES = 'node_modules/babel-core/browser-polyfill.js',
+    HTML_FILES = 'src/index.html',
+    JS_FILES = 'src/app.js';
+
 
 gulp.task("default", function () {
-  return gulp.src("src/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+  gulp.src(VENDOR_FILES)
+    .pipe(gulp.dest(DEST_VENDOR));
+
+  gulp.src(HTML_FILES)
+    .pipe(gulp.dest(DEST));
+
+  return gulp.src(JS_FILES)
+    .pipe($.babel())
+    .pipe(gulp.dest(DEST_JS));
+});
+
+gulp.task('serve', $.serve({
+    root: [DEST],
+    port: 8090
+}));
+
+gulp.task('watch', function() {
+  $.livereload.listen(8090);
+  gulp.watch([JS_FILES, HTML_FILES], ['default']);
 });
